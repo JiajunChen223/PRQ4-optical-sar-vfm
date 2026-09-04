ed2719fcf98337694fc2c401621a380dec4d04a9
JiajunChen223 <chenjiajun232@gmail.com>
2026-09-04 08:45:19 +0800

    r21: expose ICE backend, certification and profiling APIs

--- 02_experiment/code/src/geotoken3path/execution/__init__.py
+++ 02_experiment/code/src/geotoken3path/execution/__init__.py
@@ -1,13 +1,30 @@
 """Execution backends for audited foundation-model inference/training graphs."""
 
+from .certification import (
+    TensorEquivalence,
+    compare_gradients,
+    compare_tensors,
+    named_trainable_gradients,
+)
 from .contracts import BackboneFeatureContract, CromaExecutionContractError
+from .croma_executor import (
+    InterfaceCertifiedCromaExecutor,
+    install_ice_exact_forward,
+)
 from .croma_plan import CromaExecutionPlan, compile_croma_execution_plan
-from .croma_executor import InterfaceCertifiedCromaExecutor
+from .profiling import LatencySummary, profile_cuda_callable
 
 __all__ = [
     "BackboneFeatureContract",
     "CromaExecutionContractError",
     "CromaExecutionPlan",
     "InterfaceCertifiedCromaExecutor",
+    "LatencySummary",
+    "TensorEquivalence",
+    "compare_gradients",
+    "compare_tensors",
     "compile_croma_execution_plan",
+    "install_ice_exact_forward",
+    "named_trainable_gradients",
+    "profile_cuda_callable",
 ]
